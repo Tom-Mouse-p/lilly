a8b3adfcc3d1efc06a31c4a5cf395c23d46d7cb9
Pranav Rajeevan <pranav24rajeevan@gmail.com>
2023-11-04 03:10:13 +0530

    added help to display the possible commands

--- app/Components/RecordingBar.tsx
+++ app/Components/RecordingBar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 
 import { useEffect, useState } from "react";
+import CommandsTable from "./CommandsTable";
 
 interface Props {
     mode: any;
@@ -32,9 +33,28 @@ function RecordingBar({ mode, setMode }: Props) {
     function swapInput() {
         handleChangeImage();
     }
+    function toggleHelp() {
+        const helpBox = document.getElementById("helpBox");
+        helpBox?.classList.toggle("displayNone");
+    }
 
     return (
         <div className="fixed bottom-0 w-full h-28 flex items-center justify-center ">
+            <div
+                id="helpBox"
+                className="z-10 fixed h-screen w-screen bg-slate-700 top-0 left-0 flex justify-center items-center overflow-scroll"
+            >
+                <div className="flex flex-col items-end overflow-x-auto mx-2">
+                    <button
+                        type="button"
+                        className="btn bg-slate-900"
+                        onClick={toggleHelp}
+                    >
+                        Close
+                    </button>
+                    <CommandsTable />
+                </div>
+            </div>
             <div
                 id="bottom_box"
                 className=" bg-slate-950 w-full sm:w-3/4 md:w-1/2 md:max-w-2xl h-full flex justify-between items-center rounded-lg px-12 "
@@ -69,12 +89,17 @@ function RecordingBar({ mode, setMode }: Props) {
                     </button>
                 </div>
 
-                <div id="rightBox" className="flex justify-center items-center">
+                <div
+                    id="rightBox"
+                    className="flex justify-center items-center cursor-pointer"
+                >
                     <Image
-                        src="/icons/gear-solid.svg"
-                        width={20}
-                        height={20}
+                        src="/icons/help.svg"
+                        width={30}
+                        height={30}
                         alt=""
+                        onClick={toggleHelp}
+                        title="Help | Display Commands"
                     />
                 </div>
             </div>
